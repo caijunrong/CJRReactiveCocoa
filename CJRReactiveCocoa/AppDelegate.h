4b1649483973924423bd552c2b345e2d561684d1
caijunrong <biyabi@caijunrong.local>
2015-11-09 15:13:10 +0800

    Initial Commit

---
+++ CJRReactiveCocoa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CJRReactiveCocoa
+//
+//  Created by biyabi on 15/11/9.
+//  Copyright © 2015年 biyabi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
